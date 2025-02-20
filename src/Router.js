b260b2b0b4b395505524a8b8b5abb39d8c7605e5
Murali v <v.murali1818@gmail.com>
2025-02-20 17:54:03 +0530

    jobs completed

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Header from "./pages/header/Header";
 import Jobs from "./pages/Jobs/Jobs";
 import AddJob from "./pages/Jobs/AddJob";
 import JobDetails from "./pages/Jobs/JobDetails";
 import EditJob from "./pages/Jobs/EditJob";
+import { useUser } from "./contexts/UserContext";
 
 const Router = () => {
+  const { user } = useUser(); 
+  const navigate = useNavigate();
+  if (!user) {
+    return (
+        <div className="no-user" >
+           
+            <p style={{color:"white"}}>User Not found!!! Please log in to continue.</p>
+            <button onClick={navigate("/")}></button>
+          
+        </div>
+    );
+}
   return (
     
     <div className="bg-bgDark ">
@@ -20,7 +33,7 @@ const Router = () => {
       <Route path="/job-details/:id" element={<JobDetails></JobDetails>} />
       <Route path="/edit-job/:id" element={<EditJob></EditJob>} />
     </Routes>
-  </div>
+    </div>
     
   );
 };
--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -3,8 +3,8 @@ import { Client, Account } from "appwrite";
 const client = new Client();
  
 client
-  .setEndpoint("https://cloud.appwrite.io/v1") // ✅ Replace with your Appwrite endpoint
-  .setProject("67b6ce100032bb22257f"); // ✅ Replace with your Project ID
+  .setEndpoint("https://cloud.appwrite.io/v1") // 
+  .setProject("67b6ce100032bb22257f"); //
  
 const account = new Account(client);
  
